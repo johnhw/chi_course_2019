f193f57384ee3207cbfd0eae2eb0058057ec1505
John Williamson <johnhw@gmail.com>
2019-05-05 11:20:01 +0100

    Added logging and tracking example

--- 01_intro_to_bayesian_methods/src/intermittent_slider.py
+++ 01_intro_to_bayesian_methods/src/intermittent_slider.py
@@ -6,27 +6,32 @@ import numpy.ma as ma
 import time
 import slider_filter
 import sys
+import os
+import csv
+
 
 class Gilbert(object):
     """implementS a Gilbert Markov chain, which can switch between two states. Simulates
-    bursty behaviour."""    
+    bursty behaviour."""
+
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-        self.state = 0        
-    
+        self.state = 0
+
     def update(self, dt):
-        # account for sampling rate              
-        p1 = 1-((1-self.p1)**(dt))
-        p2 = 1-((1-self.p2)**(dt))
-        
-        if self.state==0:            
-            if np.random.random()<p1:
+        # account for sampling rate
+        p1 = 1 - ((1 - self.p1) ** (dt))
+        p2 = 1 - ((1 - self.p2) ** (dt))
+
+        if self.state == 0:
+            if np.random.random() < p1:
                 self.state = 1
-        if self.state==1:            
-            if np.random.random()<p2:
+        if self.state == 1:
+            if np.random.random() < p2:
                 self.state = 0
         return self.state
+
 
 class Box:
     def __init__(self, name, left, right, prior, color):
@@ -89,6 +94,24 @@ def box_probability(particles, weights, boxes):
     return probs / np.sum(probs)
 
 
+class SliderLogger:
+    def __init__(self, fields):
+        time_name = "slider_{time}.csv".format(
+            time=time.asctime().replace(" ", "_").replace(":", "_")
+        )
+        fname = os.path.join("..", "captured_data", time_name)
+        self.log_file = open(fname, "w")        
+        self.writer = csv.DictWriter(self.log_file, fieldnames=fields)
+        self.writer.writeheader()
+
+
+    def log(self, **fields):
+        self.writer.writerow(fields)
+
+    def close(self):
+        self.log_file.close()
+
+
 class SliderDemo(object):
     def __init__(self, boxes, args):
         self.boxes = boxes
@@ -102,6 +125,20 @@ class SliderDemo(object):
             h=self.slider_height,
         )
 
+        self.logger = SliderLogger(
+            fields=[
+                "t",
+                "true_x",
+                "true_dx",
+                "obs_x",
+                "obs_speed",
+                "est_x",
+                "est_dx",
+                "std_x",
+                "std_dx",
+                "mode",
+            ]
+        )
         self.gilbert = Gilbert(0.5, 0.3)
         self.n_particles = 200
         self.particles = slider_filter.prior(self.n_particles)
@@ -111,11 +148,13 @@ class SliderDemo(object):
         self.sampling_intermittency = args.sampling
         self.last_x = None
         self.last_t = time.time()
+        self.start_time = time.time()
         self.dx = 0
         self.x = 0
         self.position_noise = 0.03
 
     def quit(self, src):
+        self.logger.close()
         return
 
     def event(self, src, event_type, event):
@@ -123,13 +162,11 @@ class SliderDemo(object):
         pass
 
     def update_filter(self):
-        observation = [self.observed_x  if self.observed else np.nan, 
-        np.abs(self.dx)]
-        
+        observation = [self.observed_x if self.observed else np.nan, np.abs(self.dx)]
+
         self.particles, self.weights = slider_filter.filter_step(
             self.particles, observed=observation, dt=self.dt, prior_rate=0.00005
         )
-        
 
     def update_state(self, screen_x):
         t = time.time()
@@ -143,13 +180,13 @@ class SliderDemo(object):
             dt = 1 / 60.0
 
         self.gilbert.update(dt)
-
+        self.t = t
         self.last_x = self.x
         self.last_t = t
         self.dt = dt
         self.observed_x = self.x + np.random.normal(0, self.position_noise)
 
-        if self.gilbert.state==0:
+        if self.gilbert.state == 0:
             self.observed = np.random.uniform(0, 1) < 0.9
         else:
             self.observed = np.random.uniform(0, 1) < self.sampling_intermittency
@@ -186,30 +223,50 @@ class SliderDemo(object):
                     outline=box.color,
                 )
 
-
         expected_pos = slider_filter.expected_position(self.particles, self.weights)
         expected_var = slider_filter.variance_position(self.particles, self.weights)
         screen_expected = expected_pos[0] * self.screen_size
-        
-        
-        screen_left_var = (expected_pos[0] - np.sqrt(expected_var[0])) * self.screen_size
-        screen_right_var = (expected_pos[0] + np.sqrt(expected_var[0])) * self.screen_size
+
+        # log one row
+        self.logger.log(
+            t=time.time()-self.start_time,
+            true_x=self.x,
+            true_dx=self.dx,
+            obs_x=np.nan if not self.observed else self.observed_x,
+            obs_speed=np.abs(self.dx),
+            est_x=expected_pos[0],
+            est_dx=expected_pos[1],
+            std_x=np.sqrt(expected_var[0]),
+            std_dx=np.sqrt(expected_var[1]),
+            mode=self.gilbert.state,
+        )
+
+        screen_left_var = (
+            expected_pos[0] - np.sqrt(expected_var[0])
+        ) * self.screen_size
+        screen_right_var = (
+            expected_pos[0] + np.sqrt(expected_var[0])
+        ) * self.screen_size
 
         if self.show_block:
 
-            for std, height,stipple in [(0.5, 0.75, 'gray50'), (1, 0.5, 'gray25'), (2, 0.25, 'gray25')]:
-                left = (expected_pos[0] - np.sqrt(expected_var[0]) * std) * self.screen_size
-                right = (expected_pos[0] + np.sqrt(expected_var[0]) * std) * self.screen_size
-                top =  (0.5 - 0.5 * height) * self.slider_height
+            for std, height, stipple in [
+                (0.5, 0.75, "gray50"),
+                (1, 0.5, "gray25"),
+                (2, 0.25, "gray25"),
+            ]:
+                left = (
+                    expected_pos[0] - np.sqrt(expected_var[0]) * std
+                ) * self.screen_size
+                right = (
+                    expected_pos[0] + np.sqrt(expected_var[0]) * std
+                ) * self.screen_size
+                top = (0.5 - 0.5 * height) * self.slider_height
                 bottom = (0.5 + 0.5 * height) * self.slider_height
-                
-                src.rectangle(
-                    left, top, right, bottom,
-                    fill="blue",
-                    stipple=stipple,
-                    width=0                
-                )
 
+                src.rectangle(
+                    left, top, right, bottom, fill="blue", stipple=stipple, width=0
+                )
 
         if self.show_particles:
             for particle, w in zip(self.particles, self.weights):
@@ -218,13 +275,7 @@ class SliderDemo(object):
                 w = w if np.isfinite(w) else 0
                 src.circle(x, self.slider_height // 2, np.sqrt(w) * 20 + 1, fill="grey")
 
-            
-            src.circle(
-                screen_expected,
-                self.slider_height // 2,
-                5,
-                fill="blue",
-            )
+            src.circle(screen_expected, self.slider_height // 2, 5, fill="blue")
             src.line(
                 screen_expected,
                 0,
@@ -234,21 +285,25 @@ class SliderDemo(object):
                 width=2,
             )
 
-
             src.line(
-                screen_left_var, 0.25*self.slider_height, screen_left_var, 0.75*self.slider_height,
-                fill="blue"
+                screen_left_var,
+                0.25 * self.slider_height,
+                screen_left_var,
+                0.75 * self.slider_height,
+                fill="blue",
             )
             src.line(
-                screen_right_var, 0.25*self.slider_height, screen_right_var, 0.75*self.slider_height,
-                fill="blue"
+                screen_right_var,
+                0.25 * self.slider_height,
+                screen_right_var,
+                0.75 * self.slider_height,
+                fill="blue",
             )
-
 
         if self.observed:
             observed = self.observed_x * self.screen_size
             src.line(observed, 0, observed, self.slider_height, fill="white", width=5)
-            
+
 
 def create_slider():
     boxes = [
